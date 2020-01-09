132283cd14b8a6cb044d2a14f88b98995ebc1c5f
Jessica Bailey <JessBailey@Jessicas-MacBook-Air.local>
2020-01-09 12:13:19 +0000

    heroku test

--- create_table.py
+++ create_table.py
@@ -6,7 +6,7 @@ cursor = connection.cursor()
 
 # MUST BE INTEGER
 # This is the only place where int vs INTEGER matters—in auto-incrementing columns
-create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text)"
+create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text UNIQUE, email text UNIQUE, age INTEGER, password text, location text, option_1 text, option_2 text, option_3 text, option_4 text)"
 cursor.execute(create_table)
 
 create_table = "CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY, name text PRIMARY KEY, price real)"
