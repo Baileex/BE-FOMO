897db2d3f669a5d968754552787802b34cbd3442
Jessica Bailey <JessBailey@Jessicas-Air.home>
2020-01-12 16:28:17 +0000

    manual merges

--- models/user1.py
+++ models/user1.py
@@ -19,13 +19,6 @@ class UserModel(db.Model):
     family = db.Column(db.Boolean)
     gender = db.Column(db.String(80))
 
-    @staticmethod
-    def generate_hash(password):
-        return sha256.hash(password)
-    
-    @staticmethod
-    def verify_hash(password, hash):
-        return sha256.verify(password, hash)
 
     def __init__(self, username, password, email, age, location, option_1, option_2, option_3, option_4, family, gender):
         self.username = username
@@ -50,7 +43,15 @@ class UserModel(db.Model):
     def delete_from_db(self):
         db.session.delete(self)
         db.session.commit()
-
+    
+    
+    @staticmethod
+    def generate_hash(password):
+        return sha256.hash(password)
+    
+    @staticmethod
+    def verify_hash(password, hash):
+        return sha256.verify(password, hash)
     @classmethod
     def find_by_username(cls, username):
         return cls.query.filter_by(username=username).first()
