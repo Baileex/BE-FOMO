dd91047ca3e345a1d15731ad27b66e3deb242410
Jessica Bailey <JessBailey@Jessicas-Air.home>
2020-01-12 13:47:09 +0000

    added cors to hosted

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 from flask import Flask
-# from flask_cors import CORS
+from flask_cors import CORS
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
@@ -12,7 +12,7 @@ from resources.business import BusinessRegister, ChangeBusDetails, ChangeBusUser
 from resources.methods import Methods
 
 app = Flask(__name__)
-# cors = CORS(app)
+cors = CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db') 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
