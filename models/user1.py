d3765c318af7758d5cf8bed42e2e6e9efd8601e0
Jessica Bailey <JessBailey@Jessicas-Air.home>
2020-01-12 16:53:06 +0000

    manual merges

--- models/user1.py
+++ models/user1.py
@@ -53,6 +53,7 @@ class UserModel(db.Model):
     @staticmethod
     def verify_hash(password, hash):
         return sha256.verify(password, hash)
+        
     @classmethod
     def find_by_username(cls, username):
         return cls.query.filter_by(username=username).first()
