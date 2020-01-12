d0a17a60146de20147ec74d2b9ff78fe8531935a
Jessica Bailey <JessBailey@Jessicas-Air.home>
2020-01-12 17:13:15 +0000

    manual merges

--- resources/user1.py
+++ resources/user1.py
@@ -43,7 +43,9 @@ class UserRegister(Resource):
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
 
-        user = UserModel(data['username'], data['password'], data['email'], data['age'], data['location'], data['option_1'], data['option_2'], data['option_3'], data['option_4'], data['family'], data['gender'])
+        password = UserModel.generate_hash(data['password'])
+
+        user = UserModel(data['username'], password, data['email'], data['age'], data['location'], data['option_1'], data['option_2'], data['option_3'], data['option_4'], data['family'], data['gender'])
         
         user.save_to_db()
 
