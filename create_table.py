f27e8265cb1919a3482687b811d3efbd4ebae082
Thomas Crowther <tom.crowther1992@gmail.com>
2020-01-13 11:35:23 +0000

    added event history capabilities

--- create_table.py
+++ create_table.py
@@ -6,7 +6,7 @@ cursor = connection.cursor()
 
 # MUST BE INTEGER
 # This is the only place where int vs INTEGER matters—in auto-incrementing columns
-create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text UNIQUE, password text, email text UNIQUE, age INT, location text, option_1 text, option_2 text, option_3 text, option_4 text, family BOOLEAN DEFAULT false, gender text)"
+create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text UNIQUE, password text, email text UNIQUE, age text, location text, option_1 text, option_2 text, option_3 text, option_4 text, family BOOLEAN DEFAULT false, gender text)"
 cursor.execute(create_table)
 
 create_table = "CREATE TABLE IF NOT EXISTS businesses (id INTEGER PRIMARY KEY, username text UNIQUE, password text, email text UNIQUE, business_name text, address text, description text, url text, avatar text)"
@@ -15,8 +15,8 @@ cursor.execute(create_table)
 create_table = "CREATE TABLE IF NOT EXISTS events (id INTEGER PRIMARY KEY, name text, location text, description text, event_type text, date Date, time Time, min_age text, cost text, business_id INTEGER)"
 cursor.execute(create_table)
 
-# create_table = "CREATE TABLE IF NOT EXISTS event_history (id INTEGER PRIMARY KEY, age INTEGER, sex)"
-# cursor.execute(create_table)
+create_table = "CREATE TABLE IF NOT EXISTS event_history (id INTEGER PRIMARY KEY, age TEXT, sex TEXT, event_type TEXT, location TEXT, time TEXT)"
+cursor.execute(create_table)
 
 connection.commit()
 
