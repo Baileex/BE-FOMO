97b0e4c34fef0b4e92caf67a5fc8777a05ae4c55
Jessica Bailey <JessBailey@Jessicas-MacBook-Air.local>
2020-01-13 10:27:50 +0000

    Delete business method fixed

--- resources/methods.py
+++ resources/methods.py
@@ -18,7 +18,11 @@ class Methods(Resource):
                     "option_1": "Food",
                     "option_2": "Music",
                     "option_3": "Drinking",
-                    "option_4": "Family"
+                    "option_4": "Dance"
+                    "family":
+                    "DEFAULT false"
+                    "gender":
+                    "male"
                 }
             },
         
@@ -26,23 +30,42 @@ class Methods(Resource):
         "description": "Get User by username"
             },
 
-        "PATCH /users/username": {
-        "description": "Patch User by username -  can patch location and quiz options",
-        "example send": {
-            "location": "Manchester",
-            "option_1": "Food",
-            "option_2": "Music",
-            "option_3": "Drinking",
-            "option_4": "Family"
+        "PATCH /users/:username/location": {
+        "description": "Patch User by username": {
+            "username": "Manchester",
+
         }
     },
 
-        "DELETE /users/username": {
+        "DELETE /users/:username": {
         "description": "Delete User by username"
     },
 
 
-        "POST /auth": {
-        "description": "check username and password"
-    }
+        "POST /login": {
+        "description": "check username and password and receive an access key"
+    },
+
+       "POST /logout": {
+        "description": "check username and password and receive and blacklist access key REQUIRED" 
+       },
+
+       "PATCH /users/:username/username": {
+        "description": "Patch User Quiz results by username": {
+            "option_1": "test",
+            "option_2": "test",
+            "option_3": "test",
+            "option_4": "test"
+        }
+    },
+    
+      "DELETE /users/:username": {
+        "description": "Delete user"
+      },
+
+      "PATCH /users/:username/password": {
+          "description": "Change user password"
+      },
+
+      
     }
