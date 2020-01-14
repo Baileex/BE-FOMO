e2207d42388d37cfbac1b63b32e100181238cb50
Jessica Bailey <JessBailey@Jessicas-MacBook-Air.local>
2020-01-14 11:17:10 +0000

    starting changes

--- resources/business.py
+++ resources/business.py
@@ -68,7 +68,8 @@ class BusinessLogin(Resource):
         if business and sha256_crypt.verify(orig, business.password):
             access_token = create_access_token(identity=business.id, fresh=True)
             refresh_token = create_refresh_token(business.id)
-            return business.json() and {
+            return {
+                "details": business.json(),
                 'message': 'Business User logged in',
                 'access_token': access_token,
                 'refresh_token': refresh_token
